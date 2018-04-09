44b746fba736758e0d332fd00042e5171d3596df
Alex <alexqizixi@gmail.com>
2018-04-09 00:08:07 -0700

    added rymatrix implementation, but have issue with the output

--- BST234_project.py
+++ BST234_project.py
@@ -2,30 +2,66 @@ import numpy as np
 import time
 
 
-X = np.loadtxt("simulated_genos", delimiter=" ", dtype='int32')  # dtype?
+def prep_data():
+    x = np.loadtxt("simulated_genos", delimiter=" ", dtype="float32")
+    y = np.array([[1] * 10000 + [0] * 10000], dtype="float32")
+    y_c = y - 0.5
+    return x, y_c
 
-print(X.shape)
 
-Y = np.array([[1] * 10000 + [0] * 10000])
-print(Y.shape)
+def main():
+    x, y_c = prep_data()
+    start_time = time.clock()
+    xxt = np.dot(x, x.T)
+    end_time = time.clock()
+    print('xxt computation time: ', end_time - start_time)
+    start_time = time.clock()
+    ry_matrix = RYSparseMatrix(xxt)
+    end_time = time.clock()
+    print('ry matrix load time: ', end_time - start_time)
+    start_time = time.clock()
+    # q = (y_c.dot(xxt) * y_c).sum(axis=1)
+    # q0 = y_c.dot(xxt)
+    q0 = ry_matrix.multiply_y_c(y_c)
+    q1 = (q0 * y_c).sum(axis=1)
+    end_time = time.clock()
+    print('ry q computation time: ', end_time - start_time)
+    start_time = time.clock()
+    # q = (y_c.dot(xxt) * y_c).sum(axis=1)
+    q0 = y_c.dot(xxt)
+    q2 = (q0 * y_c).sum(axis=1)
+    end_time = time.clock()
+    print('original q computation time: ', end_time - start_time)
+    if q1 == q2:
+        print('computation results are the same')
+    else:
+        print('computation results are different')
+    print('q1: ', q1)
+    print('q2: ', q2)
 
-Y_c = Y - 0.5
 
-print(Y_c.view())
+class RYSparseMatrix(object):
+    def __init__(self, matrix):
+        matrix = matrix.T
+        h, w = matrix.shape
+        result = []
+        for i in range(h):
+            temp = []
+            matrix_line = matrix[i].tolist()
+            for index, value in enumerate(matrix_line):
+                if value > 0.5:
+                    temp.append(index)
+            result.append(temp)
+        self.matrix = result
 
-start_CPU = time.clock()
-XXt = np.dot(X, X.T)  #X %*% X.t
-end_CPU = time.clock()
+    def multiply_y_c(self, y_c):
+        y_c_p = y_c.tolist()
+        result = [[0]*20000]
+        for i, line in enumerate(self.matrix):
+            for index in line:
+                result[0][i] += y_c_p[0][index]
+        return np.array(result, dtype="float32")
 
-print((XXt.shape, (end_CPU - start_CPU)))
 
-start_CPU = time.clock()
-Q = (Y_c.dot(XXt) * Y_c).sum(axis=1)  # Calculate the quadratic form has many ways. This might not be the fastest.
-end_CPU = time.clock()
-print((Q, (end_CPU - start_CPU)))
-
-start_CPU = time.clock()
-Y_c1 = np.random.permutation(Y_c.T)  # Try to shuffle the vector
-end_CPU = time.clock()
-print(end_CPU-start_CPU)
-print(Y_c1)
+if __name__ == '__main__':
+    main()
